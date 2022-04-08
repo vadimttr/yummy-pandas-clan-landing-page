ba7ef19f3a5dc838d975d6636bee0abd4df703f8
Vince <sikatunar@gmail.com>
2022-04-09 03:32:34 +0800

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/index'
 
+import "swiper/css/bundle";
+
+
 function App() {
   return (
     <Router>
